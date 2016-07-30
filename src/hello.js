01435a91844b6ba62ff071b7e3c3625d459ac8ff
Peng.Li <peng.li@rea-group.com>
2016-07-30 15:22:59 +0800

    fix zipcode-to-barcode specs and refactor code

--- src/hello.js
+++
@@ -1,9 +0,0 @@
-let core = require('./core');
-
-module.exports = function () {
-  core();
-  let h = "Hello,";
-  let w = "world!";
-  console.log("=========== hello =========");
-  return h + w;
-};
